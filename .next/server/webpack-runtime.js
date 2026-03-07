9625a0f151ba0b773c714b29ea6e0a200630d5a3
Javier Jiménez <javiaservicios@gmail.com>
2026-03-08 00:48:15 +0100

    fix: remove fallback mock data since n8n internal webhooks are repaired

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c9e8c1ab3c850174")
+/******/ 		__webpack_require__.h = () => ("10f3ff9a813254cc")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
